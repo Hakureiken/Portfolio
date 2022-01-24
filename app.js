c4d81a807a32a01510ee1471135d6c3cf6571e0d
kevin gaste <kevin.gaste10@gmail.com>
2022-01-24 19:57:25 +0100

    portfolio

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-console.log(document.querySelector('#containermonnaie'));
 
 let startrendu = document.querySelector("#monnaie");
 
@@ -148,7 +147,113 @@ document.querySelector('#startechec').addEventListener('click', function(){
                     }
             }
     }
+
     console.log(booleenend);
     document.querySelector('#echec #result').insertAdjacentHTML("beforebegin",`<div class="boolean text-center">Résultat du check : ${booleenend}</div>`)
     return (booleenend);
 })
+
+// switch du texte en anglais/français
+let first = "Qui suis-je?"
+    let first1 = "Who am I?"
+let second = "Je suis passionné par l'Astronomie, les Mathématiques, l'Informatique ainsi que par les Mangas. Lors de mon travail en espace vert, j'ai décidé de me lancer en tant que développeur web j'ai donc suivi la formation RAN PRO métier du numérique et aussi réalisé des petits projets de mon côté."
+    let second1 = "I'm passionate by astronomy, Mathematics, computer, manga. When working in green space, i decided to start a new job as developer web si followed the RAN PRO digital and i also make some little project on my own"
+let third = "Mes créations."
+    let third1 = "My creations."
+let fourth = "Avec 2 collègues nous avons fait le site d'un bar à chats sur Troyes lors de notre stage pour la formation RAN PRO métier du numérique, Troyespetitschats sur Wordpress."
+    let fourth1 = "With 2 colleagues we make a cat bar site in Troyes on Wordpress during our 'stage' for the formation RAN PRO."
+let fifth = "Pour m'entrainer après la formation, j'ai décidé de réaliser ce petit projet de design entre autre pour le côté responsive."
+    let fifth1 = "For training during my formation, i decided to make a little design project responsive."
+let sixth = "Projet en cours, pour m'entrainer mais cette fois on va toucher au javascript surtout et au design, le responsive viendra à la fin."
+    let sixth1 = "Project in progress, training with javaScript and design. I'll make the responsive at the end."
+let seventh = "Exercice d'algorithme."
+    let seventh1 = "Algorythm exercice."
+let eighth = "Pour cet algorithme, vous devez placer les différentes pièces noires ( p (pion), f (fou), d (dame), r (roi), t (tour), c (cavalier)) sur les cases présente de l'échiquier. Ensuite vous placerez le cavalier blanc (C (cavalier)). Vous verrez alors, si celui-ci est capable de capturer une pièce noire."
+    let eighth1 = "For this algorythm, you have to place different black piece in the chess (p (pawn), f (bishop), d (queen), r (king), t (rook), c (knight)). When you're done, place a white knight (C (knight)). You'll see if the white knight is able to take a black piece."
+let nineth = "Lancez le test pour savoir si un cavalier blanc (C) peut prendre une pièce noire."
+    let nineth1 = "Run the test to see if a white knight (C) can take a black piece."
+let tenth = "Exercice du rendue de monnaie."
+    let tenth1 = "Exercice change."
+let eleventh = "Ici, vous pouvez tester un algorithme que j'ai fait pendant la formation à la RAN PRO en tant qu'exercice. Attention, si vous lancez l'algorithme, ne changez pas d'onglet sous peine de faire échouer le test, merci."
+    let eleventh1 = "Here, you can test an algorythm i make when i was in formation RAN PRO. WARNING, if you want to try it, don't change tab, the test will fails."
+let twelfth = "Testez ici l'algorithme de rendue de monnaie."
+    let twelfth1 = "Click here to test this algorythm."
+function modifyText(id) {
+    let switchText = document.getElementById(id)
+
+    if (switchText.firstChild.nodeValue === first) {
+        switchText.firstChild.nodeValue = first1
+    } else if (switchText.firstChild.nodeValue === first1) {
+        switchText.firstChild.nodeValue = first
+    }
+
+    if (switchText.firstChild.nodeValue === second) {
+        switchText.firstChild.nodeValue = second1
+    } else if (switchText.firstChild.nodeValue === second1) {
+        switchText.firstChild.nodeValue = second
+    }
+
+    if (switchText.firstChild.nodeValue === third) {
+        switchText.firstChild.nodeValue = third1
+    } else if (switchText.firstChild.nodeValue === third1) {
+        switchText.firstChild.nodeValue = third
+    }
+
+    if (switchText.firstChild.nodeValue === fourth) {
+        switchText.firstChild.nodeValue = fourth1
+    } else if (switchText.firstChild.nodeValue === fourth1) {
+        switchText.firstChild.nodeValue = fourth
+    }
+
+    if (switchText.firstChild.nodeValue === fifth) {
+        switchText.firstChild.nodeValue = fifth1
+    } else if (switchText.firstChild.nodeValue === fifth1) {
+        switchText.firstChild.nodeValue = fifth
+    }
+
+    if (switchText.firstChild.nodeValue === sixth) {
+        switchText.firstChild.nodeValue = sixth1
+    } else if (switchText.firstChild.nodeValue === sixth1) {
+        switchText.firstChild.nodeValue = sixth
+    }
+
+    if (switchText.firstChild.nodeValue === seventh) {
+        switchText.firstChild.nodeValue = seventh1
+    } else if (switchText.firstChild.nodeValue === seventh1) {
+        switchText.firstChild.nodeValue = seventh
+    }
+
+    if (switchText.firstChild.nodeValue === eighth) {
+        switchText.firstChild.nodeValue = eighth1
+    } else if (switchText.firstChild.nodeValue === eighth1) {
+        switchText.firstChild.nodeValue = eighth
+    }
+
+    if (switchText.firstChild.nodeValue === nineth) {
+        switchText.firstChild.nodeValue = nineth1
+    } else if (switchText.firstChild.nodeValue === nineth1) {
+        switchText.firstChild.nodeValue = nineth
+    }
+
+    if (switchText.firstChild.nodeValue === tenth) {
+        switchText.firstChild.nodeValue = tenth1
+    } else if (switchText.firstChild.nodeValue === tenth1) {
+        switchText.firstChild.nodeValue = tenth
+    }
+
+    if (switchText.firstChild.nodeValue === eleventh) {
+        switchText.firstChild.nodeValue = eleventh1
+        console.log(eleventh1);
+    } else if (switchText.firstChild.nodeValue === eleventh1) {
+        switchText.firstChild.nodeValue = eleventh
+        console.log(eleventh);
+    }
+
+    if (switchText.firstChild.nodeValue === twelfth) {
+        switchText.firstChild.nodeValue = twelfth1
+    } else if (switchText.firstChild.nodeValue === twelfth1) {
+        switchText.firstChild.nodeValue = twelfth
+    }
+}
+
+  
